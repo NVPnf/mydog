59476a45d352ffbb249c589019ecc76b87319ac9
Natalia Purtova <3716987@gmail.com>
2020-05-16 18:39:24 +0300

    change theme button

---
+++ javascript.js/js.js
@@ -0,0 +1,17 @@
+let themeButton = document.querySelector('.changeTheme');
+themeButton.onclick = function() {
+	document.querySelector('.container').classList.toggle('light_theme');
+	document.querySelector('.container').classList.toggle('dark_theme');
+};
+
+
+
+
+// let page = document.querySelector('.page');
+// let themeButton = document.querySelector('.theme-button');
+
+// themeButton.onclick = function() {
+//   page.classList.toggle('light-theme');
+// };
+
+
